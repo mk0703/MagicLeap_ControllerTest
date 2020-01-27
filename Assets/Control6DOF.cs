c3436dcd986d9d08b6ab97f6632d8a9dedb69159
kohei morishita <ko_hei0703@msn.com>
2020-01-27 22:37:10 +0900

    new  create
    add first files

---
+++ Assets/Control6DOF.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private MLInputController mLInputController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLInput.Start();
+        mLInputController = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    void OnDestory()
+    {
+        MLInput.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mLInputController.Position;
+        transform.rotation = mLInputController.Orientation;
+    }
+}
